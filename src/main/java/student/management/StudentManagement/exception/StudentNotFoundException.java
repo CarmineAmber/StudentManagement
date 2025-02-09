09f99f35ed340376e251ccf9dbb0c2b462d145bc
CarmineAmber <ryota9903@fine.ocn.ne.jp>
2025-02-09 15:13:57 +0900

    エラーハンドルの作動チェック中です。

--- src/main/java/student/management/StudentManagement/exception/StudentNotFoundException.java
+++ src/main/java/student/management/StudentManagement/exception/StudentNotFoundException.java
@@ -2,6 +2,6 @@ package student.management.StudentManagement.exception;
 
 public class StudentNotFoundException extends RuntimeException {
     public StudentNotFoundException(String message) {
-        super();
+        super(message);
     }
 }
--- src/main/java/student/management/StudentManagement/exception/StudentRegistrationException.java
+++ src/main/java/student/management/StudentManagement/exception/StudentRegistrationException.java
@@ -2,6 +2,6 @@ package student.management.StudentManagement.exception;
 
 public class StudentRegistrationException extends RuntimeException{
      public StudentRegistrationException(String message){
-         super();
+         super(message);
     }
 }
